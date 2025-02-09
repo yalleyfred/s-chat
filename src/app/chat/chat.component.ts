72d3a1c8ff8bb1bb4598a2eebccbe951dadf1ae9
Fredrick Yalley <94568827+yalleyfred@users.noreply.github.com>
2025-02-09 21:10:18 +0000

    add chat component

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-chat',
+  imports: [MatIcon],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.scss'
+})
+export class ChatComponent {
+
+}
